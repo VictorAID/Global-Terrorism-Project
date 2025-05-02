604138e391fd6e984ff4195b3913f2917f40423c
VictorAID <75183450+VictorAID@users.noreply.github.com>
2025-05-02 21:33:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -169,4 +169,4 @@ def update_dashboard(regions, countries, attacks, years):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8050))
-    app.run_server(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=False)
